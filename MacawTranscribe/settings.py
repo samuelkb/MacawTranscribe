83f921108be4d2bd8d4f847771abd3ced41232d5
Samuelkb <samuelkb.hercar@gmail.com>
2026-03-27 22:32:16 -0600

    Feat: Tailwind and CSS tokens for UI

--- MacawTranscribe/settings.py
+++ MacawTranscribe/settings.py
@@ -140,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 LOG_LEVEL = env('LOG_LEVEL', default='INFO')
 LOGS_DIR = BASE_DIR / 'logs'
